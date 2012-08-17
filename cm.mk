390ffe014f781c23574c4d8459c04be441c68866
djMesias <djmesias@terra.es>
2012-08-17 20:00:51 +0200

    Eliminando información de producto
    Change-Id: I3c53b96f13b682a69ce5a4957eab98e705fa96e0

--- cm.mk
+++ cm.mk
@@ -17,7 +17,4 @@ PRODUCT_MANUFACTURER := HTC
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bravo \
-    BUILD_ID=IMM76L \
-    BUILD_FINGERPRINT=google/bravo/bravo:4.0.4/IMM76L/299849:userdebug/test-keys \
-    PRIVATE_BUILD_DESC="djmesias 4.0.4 IMM76L 299849 test-keys" \
-    TARGET_BUILD_TYPE=userdebug
+    BUILD_ID=IMM76L
