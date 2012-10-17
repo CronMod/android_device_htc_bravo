2aebf01521ad686bbc92e9971cfe4a595ff9c4c1
Corey Moyer <corey@CronBox4-Dev.(none)>
2012-10-17 09:07:26 -0400

    Quick fix

--- cm.mk
+++ cm.mk
@@ -19,7 +19,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=htc_wwe/htc_bravo/bravo:2.3.3/GRI40/96875.1:user/release-keys \
     BUILD_VERSION_TAGS=release-keys \
     PRIVATE_BUILD_DESC="3.14.405.1 CL96875 release-keys" \
-    PRODUCT_NAME=bravo
+    PRODUCT_NAME=htc_bravo
 
 
 
