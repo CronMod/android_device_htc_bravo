32fda5c5191d64639594f29b2151ca769055f8f1
Corey Moyer <corey@CronBox4-Dev.(none)>
2012-12-30 16:27:12 -0500

    Add gingerbread branch from cyanogenmod

--- bravo.mk
+++ bravo.mk
@@ -19,50 +19,111 @@
 # not specialized for any geography.
 #
 
-# First, the most specific values, i.e. the aspects that are specific to GSM
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
 
-DEVICE_PACKAGE_OVERLAYS := device/htc/bravo/overlay
-
-#
-# Packages required for bravo
-#
-
-# Sensors
-PRODUCT_PACKAGES += \
-    gps.bravo \
-    lights.bravo \
-    sensors.bravo \
-    camera.qsd8k
-
-# Ramdisk
 PRODUCT_COPY_FILES += \
-    device/htc/bravo/prebuilt/init.bravo.rc:root/init.bravo.rc \
-    device/htc/bravo/prebuilt/init.bravo.usb.rc:root/init.bravo.usb.rc \
-    device/htc/bravo/prebuilt/ueventd.bravo.rc:root/ueventd.bravo.rc
+    device/htc/bravo/init.bravo.rc:root/init.bravo.rc
 
-# Prebuilt files/configs
-PRODUCT_COPY_FILES += \
-    device/htc/bravo/prebuilt/bravo-keypad.kl:system/usr/keylayout/bravo-keypad.kl \
-    device/htc/bravo/prebuilt/bravo-keypad.kcm:system/usr/keychars/bravo-keypad.kcm \
-    device/htc/bravo/prebuilt/curcial-oj.idc:system/usr/idc/curcial-oj.idc \
-    device/htc/bravo/prebuilt/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-    device/htc/bravo/prebuilt/h2w_headset.kcm:system/usr/keychars/h2w_headset.kcm \
-    device/htc/bravo/prebuilt/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
-    device/htc/bravo/prebuilt/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
-    device/htc/bravo/prebuilt/synaptics-rmi-touchscreen.kcm:system/usr/keychars/synaptics-rmi-touchscreen.kcm \
-    device/htc/bravo/prebuilt/vold.fstab:system/etc/vold.fstab \
-    device/htc/bravo/prebuilt/sysctl.conf:system/etc/sysctl.conf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    rild.libpath=/system/lib/libhtc_ril.so \
+    ro.ril.ecc.HTC-ELL=92,93,94 \
+    ro.ril.ecc.HTC-WWE=999 \
+    ro.ril.enable.a52.HTC-ITA=1 \
+    ro.ril.enable.a53.HTC-ITA=1 \
+    ro.ril.enable.a52=0 \
+    ro.ril.enable.a53=1 \
+    ro.ril.enable.dtm=1 \
+    ro.ril.gprsclass=12 \
+    ro.ril.hsdpa.category=8 \
+    ro.ril.hsupa.category=5 \
+    ro.ril.hsxpa=2 \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=15 \
+    mobiledata.interfaces=rmnet0,rmnet1,rmnet2
 
-# Permissions
+# Default network type.
+# 0 => WCDMA preferred.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=0
+
+# Set default_france.acdb to audio_ctl driver if the ro.cid is HTC__203
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.prl.recognition=1
+
+# AGPS otpions
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.def.agps.mode=2
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072
+
+# This is a high density device with more memory, so larger vm heaps for it.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapsize=48m
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/bravo/bravo-vendor.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.media.dec.jpeg.memcap=20000000
+
+# Don't set /proc/sys/vm/dirty_ratio to 0 when USB mounting
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/bravo/overlay
+
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
 
 # media config xml file
 PRODUCT_COPY_FILES += \
-    device/htc/bravo/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+    device/htc/bravo/media_profiles.xml:system/etc/media_profiles.xml
 
-# Common qsd8k stuff
-$(call inherit-product, device/htc/qsd8k-common/qsd8k.mk)
+PRODUCT_PACKAGES += \
+    sensors.bravo \
+    lights.bravo \
+    librs_jni \
+    gralloc.qsd8k \
+    copybit.qsd8k \
+    bravo-keypad.kcm \
+    gps.bravo \
+    libOmxCore \
+    libOmxVidEnc \
+    com.android.future.usb.accessory
 
-# Also get non-open-source GSM-specific aspects if available
-$(call inherit-product-if-exists, vendor/htc/bravo/bravo-vendor.mk)
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/bravo-keypad.kl:system/usr/keylayout/bravo-keypad.kl \
+    device/htc/bravo/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/bravo/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    device/htc/bravo/vold.fstab:system/etc/vold.fstab
+
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/bcm4329.ko:system/lib/modules/bcm4329.ko \
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/htc/bravo/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
